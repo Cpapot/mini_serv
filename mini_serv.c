f35694f0e93b037f475ed80e9c30296dd6658a9b
Cpapot <corentinpapot2004@gmail.com>
2024-06-09 19:14:39 +0200

    mi

--- mini_serv.c
+++ mini_serv.c
@@ -1,51 +1,36 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <netdb.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/select.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-/*Allowed functions: write, close, select, socket, accept,
- listen, send, recv, bind, strstr, malloc, realloc, free,
- calloc, bzero, atoi, sprintf, strlen, exit, strcpy, strcat, memset
-*/
+#define	BUFF		400000
+#define	MAXUSER		69000
 
-struct clientdata
+int		FDtoID[MAXUSER];
+char	writeBUFF[BUFF + 100];
+int		maxID = 0, maxFD;
+fd_set	readFD, writeFD, actualFD;
+
+
+void printerror(char *msg)
 {
-	int		id;
-	char	*msg;
-}typedef t_clientdata;
-
-t_clientdata	clientBUFF[1024];
-fd_set readFD, actualFD, writeFD;
-char writeBUFF[400200];
-int maxFD = 0, maxID = 0;
-
-void debugprint(char *print)
-{
-	write(1, print, strlen(print));
-	write(1, "\n", 1);
+	write(2, msg, strlen(msg));
+	exit(1);
 }
 
-void sendall(int senderfd)
+void sendall(int sender)
 {
 	for (int fd = 0; fd <= maxFD; fd++)
 	{
-		if (FD_ISSET(fd, &writeFD) && fd != senderfd)
-		{
-			send(fd, &writeBUFF, strlen(writeBUFF), 0);
-		}
+		if (FD_ISSET(fd, &writeFD) && fd != sender)
+			send(fd, writeBUFF, strlen(writeBUFF), 0);
 	}
-	bzero(&writeBUFF, sizeof(char [400000]));
-}
-
-void printerror(char *message)
-{
-	write(2, message, strlen(message));
-	exit(1);
+	bzero(&writeBUFF, sizeof(writeBUFF));
 }
 
 int main(int argc, char **argv)
@@ -53,28 +38,26 @@ int main(int argc, char **argv)
 	if (argc != 2)
 		printerror("Wrong number of arguments\n");
 
-	int sockfd;
-	socklen_t len;
-	struct sockaddr_in servaddr;
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in servaddr, cli;
+	socklen_t	len = sizeof(cli);
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1)
 		printerror("Fatal error\n");
 
-	//bzero(&clientBUFF, sizeof(t_clientdata [1024]));
 	bzero(&servaddr, sizeof(servaddr));
-	FD_ZERO(&actualFD);
-	maxFD = sockfd;
-	FD_SET(sockfd ,&actualFD);
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
+	servaddr.sin_addr.s_addr = htonl(2130706433);
 	servaddr.sin_port = htons(atoi(argv[1]));
 
 	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
 		printerror("Fatal error\n");
 
-	if (listen(sockfd, 10) != 0)
+	if (listen(sockfd, 4096) != 0)
 		printerror("Fatal error\n");
+
+	maxFD = sockfd;
+	FD_ZERO(&actualFD);
+	FD_SET(sockfd, &actualFD);
 
 	while (42)
 	{
@@ -82,41 +65,43 @@ int main(int argc, char **argv)
 
 		if (select(maxFD + 1, &readFD, &writeFD, NULL, NULL) < 0)
 			continue;
+
+		if (FD_ISSET(sockfd, &readFD))
+		{
+			int newfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+			if (newfd < 0)
+				printerror("Fatal error\n");
+			if (newfd > maxFD)
+				maxFD = newfd;
+
+			FDtoID[newfd] = maxID++;
+			FD_SET(newfd, &actualFD);
+
+			sprintf(writeBUFF, "server: client %d just arrived\n", FDtoID[newfd]);
+			sendall(newfd);
+
+			continue;
+		}
 		for (int fd = 0; fd <= maxFD; fd++)
 		{
-			if (FD_ISSET(fd, &readFD) && sockfd == fd)
+			if (FD_ISSET(fd, &readFD))
 			{
-				int newfd = accept(sockfd, (struct sockaddr *)&servaddr, &len);
-				if (newfd < 0)
-					continue;
-				if (newfd > maxFD)
-					maxFD = newfd;
-				clientBUFF[newfd].id = maxID++;
-				FD_SET(newfd, &actualFD);
-				sprintf(writeBUFF, "server: client %d just arrived\n", clientBUFF[newfd].id);
-				sendall(newfd);
-				break;
-			}
-			if (FD_ISSET(fd, &readFD) && sockfd != fd)
-			{
-				char readBUFF[400000];
-				bzero(readBUFF, sizeof(readBUFF));
-				int recvbyte = 1;
-				while (recvbyte == 1 && readBUFF[strlen(readBUFF) - 1] != '\n')
-					recvbyte = recv(fd, readBUFF + strlen(readBUFF), 1, 0);
-				if (recvbyte <=0)
+				int		nbytes = 1;
+				char	readBUFF[BUFF];
+
+				bzero(&readBUFF, sizeof(readBUFF));
+				while (nbytes == 1 && readBUFF[strlen(readBUFF) - 1] != '\n')
+					nbytes = recv(fd, readBUFF + strlen(readBUFF), 1, 0);
+
+				if (nbytes <= 0)
 				{
-					sprintf(writeBUFF, "server: client %d just left\n", clientBUFF[fd].id);
-					sendall(fd);
+					sprintf(writeBUFF, "server: client %d just left\n", FDtoID[fd]);
 					FD_CLR(fd, &actualFD);
 					close(fd);
-					break ;
 				}
 				else
-				{
-					sprintf(writeBUFF, "client %d: %s", clientBUFF[fd].id, readBUFF);
-					sendall(fd);
-				}
+					sprintf(writeBUFF, "client %d: %s", FDtoID[fd], readBUFF);
+				sendall(fd);
 			}
 		}
 	}
